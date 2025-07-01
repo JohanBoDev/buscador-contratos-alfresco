4243e969aec6d63508dbaa4d9e8cd4c07f3bc394
JohanBoDev <bohorquezjohan958@gmail.com>
2025-06-30 22:55:26 -0500

    s

--- db_marcados.py
+++ db_marcados.py
@@ -1,8 +1,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), 'marcados.db')
-
+DB_PATH = os.path.join("/data", "marcados.db")
 def conectar():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
